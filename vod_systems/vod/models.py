9f7cabe94833270aa879228dd6887aaeb89ec853
hizni.salih <hizni.salih@gmail.com>
2017-08-18 15:47:32 +0100

    Modified Models - made all metadata have same structure
    Done to simplify code. Can change it back if it will be a problem...

--- vod_systems/vod/models.py
+++ vod_systems/vod/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
+# models hold meta data that is used to describe data held in primary tables
+
 
 class Institution(models.Model):
-    institution_code = models.CharField(max_length=20)
+    code = models.CharField(max_length=20)  #institution code
     description = models.CharField(max_length=100)
 
 
 class AliasIdentifier(models.Model):
-    identifier_type = models.CharField(max_length=20)
+    code = models.CharField(max_length=20) # identifier type
     description = models.CharField(max_length=100)
 
 
 class DataType(models.Model):
-    units = models.CharField(max_length=20)
+    code = models.CharField(max_length=20)  # units
     description = models.CharField(max_length=255)
 
 
 class TransplantType(models.Model):
+    code = models.CharField(max_length=20) # transplant type
     description = models.CharField(max_length=100)
 
