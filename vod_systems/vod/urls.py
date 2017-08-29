b5a63fb634cd49451e73a6b4ddef6931e3ec4c38
hizni.salih <hizni.salih@gmail.com>
2017-08-29 16:39:31 +0100

    Added user-retire form
    Set active flag for user record

--- vod_systems/vod/urls.py
+++ vod_systems/vod/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from vod import views
-from vod.views import UserListView, UserCreateView, UserUpdateView, UserDeleteView
+from vod.views import UserListView, UserCreateView, UserUpdateView, UserDeleteView, UserRetireView
 
 urlpatterns = [
     url(r'login', views.login, name='vod-login'),
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^list/$', UserListView.as_view(), name='user-list'),
     url(r'^create-user/$', UserCreateView.as_view(), name='user-create'),
     url(r'^update-user/(?P<id>\d+)/$', UserUpdateView.as_view(), name="user-update"),
+    url(r'^retire-user/(?P<id>\d+)/$', UserRetireView.as_view(), name="user-retire"),
     url(r'^delete-user/(?P<id>\d+)/$', UserDeleteView.as_view(), name="user-delete"),
 ]
