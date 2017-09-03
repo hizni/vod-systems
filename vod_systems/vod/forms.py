90495536de36544809ff8337a889fc6365221c86
hizni.salih <hizni.salih@gmail.com>
2017-09-03 21:15:19 +0100

    Setup layout of crispy-form

--- vod_systems/vod/forms.py
+++ vod_systems/vod/forms.py
@@ -1,60 +1,45 @@
-
+from crispy_forms.bootstrap import PrependedText
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
-
-from models import Patient, Institution, AliasIdentifier, DataType, TransplantType
-
-
-class InstitutionForm(ModelForm):
-    class Meta:
-        model = Institution
-        fields = ['code', 'description']
+from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 
-class AliasIdentifierForm(ModelForm):
-    class Meta:
-        model = AliasIdentifier
-        fields = ['code', 'description']
+class SimpleForm(ModelForm):
+    # helper = FormHelper()
+    # helper.form_tag = False
 
-
-class DataTypeForm(ModelForm):
-    class Meta:
-        model = DataType
-        fields = ['code', 'description']
-
-
-class TransplantTypeForm(ModelForm):
-    class Meta:
-        model = TransplantType
-        fields = ['code', 'description']
-
-
-class PatientForm(ModelForm):
-    class Meta:
-        model = Patient
-        fields = ['surname', 'firstname', 'gender', 'date_of_birth', 'date_of_death']
-
-
-# Extending the default Django UserCreationForm that just has the following fields:
-#   username, password
-class ExtendedUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(label="First name", min_length=5, max_length=15)
-    last_name = forms.CharField(label="Last name")
-    email = forms.EmailField(label="Email")
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+        fields = ['first_name', 'last_name', 'email', 'username', 'password', 'confirm_password', 'is_staff', 'is_superuser']
+        # fields = ['username', 'password', 'confirm_password']
 
-    def save(self, commit=True):
-        user = super(ExtendedUserCreationForm, self).save(commit=False)
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        user.email = self.cleaned_data["email"]
+    def __init__(self, *args, **kwargs):
+        super(SimpleForm, self).__init__(*args, **kwargs)
 
-        if commit:
-            user.save()
-        return user
+        self.helper = FormHelper()
+        self.helper.form_id = 'form'
+        self.helper.form_action = '.'
+        self.helper.form_class = 'generic-modal'
+        self.helper.attrs = {'data-validate-parsley': 'parsley'}
+        self.helper.layout = Layout(
+            Field('username',  id="username", required=True, form_class="has_warning"),
+            Field('password', id="password", required=True),
+            Field('confirm_password', id="confirm_password", required=True, data_parsley_equalto="#password"),
+        )
+        # self.helper.add_input(Submit('submit', 'Submit'))
+
+
+
+
+
+
+
+
+
+
 
